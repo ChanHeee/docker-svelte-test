dbb49be30e4c61a9bf09c69bf3d118d63a35b800
박찬희 <pch0989@gmail.com>
2022-03-03 16:20:03 +0900

    feat : Svelte 앱 생
    - svlete-test 레포지토리 복사

---
+++ README.md
@@ -0,0 +1,21 @@
+# Svelte 연습
+
+## 계좌 출입금 내역 조회 UI 구현
+
+<br>
+
+![screen](https://user-images.githubusercontent.com/19974384/155630336-471400ab-23c9-4cd3-ba6a-5e908182fada.gif)
+
+> 실행 방법
+
+1. dependencies 설치
+
+   ```bash
+   cd svelte-test
+   npm install
+   ```
+
+2. 빌드 후 실행
+   ```bash
+   npm run dev
+   ```
---
+++ src/components/Transaction.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let mode;
+  export let value;
+  export let removeTransaction;
+  export let index;
+
+  $: cssClass = getCssClass(mode);
+  const getCssClass = (mode) => {
+    switch (mode) {
+      case 'balance':
+        return 'is-info';
+      case 'income':
+        return 'is-success';
+      case 'expenses':
+        return 'is-danger';
+    }
+  };
+</script>
+
+<div class="notification {cssClass} is-light has-text-left">
+  <span>{value}</span>
+  <button
+    id={index}
+    class="delete is-medium mt-2.5 mr-2"
+    on:click={(event) => removeTransaction(event)}
+  />
+</div>
